4a91662ede72ed280484159c7a68e61969fca17d
Evgeny Stepanov <evgenos@list.ru>
2020-10-06 11:56:57 +0300

    added server

---
+++ src/server/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
+
+const PORT = process.env.PORT ? process.env.PORT : 8080;
+app.use(express.json());
+
+app.use(cors());
+
+app.use('/images', express.static(path.join(__dirname, '../client/img')));
+app.use(express.static(__dirname + '../../public'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, '../../dist')));
+  app.listen(PORT, () => {
+    console.log(`Listening port: ${PORT}`);
+  });
+} else if (process.env.NODE_ENV === 'development') {
+  const webpack = require('webpack');
+  const config = require(path.join(__dirname, '../../webpack.config'));
+  const compiler = webpack(config);
+  app.use(
+    require('webpack-dev-middleware')(compiler, {
+      publicPath: config.output.publicPath,
+      stats: 'errors-only',
+      writeToDisk: true,
+    })
+  );
+  app.use(require('webpack-hot-middleware')(compiler));
+  app.listen(PORT, () => {
+    console.log(`Server listening port: ${PORT}`);
+  });
+   app.get('/*', function (request, response){
+    response.sendFile(path.resolve(__dirname, '../../dist', 'index.html'))
+  })
+}
+  
+module.exports = app;
