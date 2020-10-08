6f403518ee294ade20763d553c370594b890922b
Evgeny Stepanov <evgenos@list.ru>
2020-10-08 15:22:23 +0300

    added Loading component

--- src/client/components/FeaturedRooms.js
+++ src/client/components/FeaturedRooms.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Title from './Title';
 import { StoreContext } from '../store';
+import Loading from './Loading';
+
 
 export default function FeaturedRooms() {
   const { featuredRooms } = React.useContext(StoreContext);
   return (
     <div className='featured-rooms'>
       <Title title='featured rooms' />
+      <Loading /> 
     </div>
   );
 }
