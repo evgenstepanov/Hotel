e1c707614aa11e947b74b324ec2520ecc710ca49
Evgeny Stepanov <evgenos@list.ru>
2020-10-08 11:08:17 +0300

    added initial processing of the data

--- src/client/store.js
+++ src/client/store.js
@@ -1,18 +1,47 @@
-import React, { useState } from 'react';
-export const StoreContext = React.createContext(null);
+import React, { useState, useEffect } from 'react';
+import items from './data';
 
+export const StoreContext = React.createContext(null);
 export default ({ children }) => {
   let initialMenu = [
     { title: 'Home', src: '/' },
     { title: 'Rooms', src: '/rooms' },
   ];
   const [menu, setMenu] = useState(initialMenu);
-
   const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const [rooms, setRooms] = useState([]);
+  const [sortedRooms, setSortedRooms] = useState([]);
+  const [featuredRooms, setFeaturedRooms] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    let rooms = formatData(items);
+    let featuredRooms = rooms.filter((room) => room.featured === true);
+    setRooms(rooms);
+    setSortedRooms(rooms);
+    setFeaturedRooms(featuredRooms);
+    setLoading(false);
+  }, []);
+
+  const formatData = () => {
+    let tempItems = items.map((item) => {
+      let id = item.sys.id;
+      let images = item.fields.images.map((image) => image.fields.file.url);
+
+      let room = { ...item.fields, images, id };
+      return room;
+    });
+    return tempItems;
+  };
 
   const store = {
     menu: [menu, setMenu],
     menuIsOpen: [menuIsOpen, setMenuIsOpen],
+    rooms: [rooms, setRooms],
+    sortedRooms: [sortedRooms, setSortedRooms],
+    featuredRooms: [featuredRooms, setFeaturedRooms],
+    loading: [loading, setLoading],
   };
   return (
     <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
