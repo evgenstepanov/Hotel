b246690fa634e67a25b30358aac53a017bf21420
Evgeny Stepanov <evgenos@list.ru>
2020-10-09 20:23:48 +0300

    added loading status

--- src/client/components/FeaturedRooms.js
+++ src/client/components/FeaturedRooms.js
@@ -2,14 +2,22 @@ import React from 'react';
 import Title from './Title';
 import { StoreContext } from '../store';
 import Loading from './Loading';
-
+import Room from './Room';
 
 export default function FeaturedRooms() {
-  const { featuredRooms } = React.useContext(StoreContext);
+  const {
+    featuredRooms: [featuredRooms],
+    loading: [loading],
+  } = React.useContext(StoreContext);
+
   return (
-    <div className='featured-rooms'>
+    <section className='featured-rooms'>
       <Title title='featured rooms' />
-      <Loading /> 
-    </div>
+      {loading ? (
+        <Loading />
+      ) : (
+        featuredRooms.map((room) => <Room key={room.id} data={room} />)
+      )}
+    </section>
   );
 }
