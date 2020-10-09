6092e4fd71e042fd1bd0faa1159ae499ce1b708c
Evgeny Stepanov <evgenos@list.ru>
2020-10-09 20:26:32 +0300

    added a logo for each service block

--- src/client/components/Services.js
+++ src/client/components/Services.js
@@ -1,43 +1,43 @@
 import React from 'react';
-import { FaBeer } from 'react-icons/fa';
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from 'react-icons/fa';
 import Title from './Title';
 
 export default function Services() {
   let services = [
     {
-      icon: <FaBeer />,
-      title: 'Serv1',
+      icon: <FaCocktail className='service__icon' />,
+      title: 'Free Cocktails',
       info:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, perferendis?',
     },
     {
-      icon: <FaBeer />,
-      title: 'Serv2',
+      icon: <FaHiking className='service__icon' />,
+      title: 'Endless Hiking',
       info:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, perferendis?',
     },
     {
-      icon: <FaBeer />,
-      title: 'Serv3',
+      icon: <FaShuttleVan className='service__icon' />,
+      title: 'Free Shuttle',
       info:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, perferendis?',
     },
     {
-      icon: <FaBeer />,
-      title: 'Serv4',
+      icon: <FaBeer className='service__icon' />,
+      title: 'Strongest Beer',
       info:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, perferendis?',
     },
   ];
 
   return (
-    <section>
+    <section className='services'>
       <Title title='Services'></Title>
-      <div className='services'>
+      <div className='center-container'>
         {services.map((item) => {
           return (
             <article key={item.title} className='service'>
-              <span className='service__icon'>{item.icon}</span>
+              {item.icon}
               <h4 className='service__title'>{item.title}</h4>
               <p className='service__info'>{item.info}</p>
             </article>
