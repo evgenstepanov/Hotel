cd7b2be0cd7e66f070c26fcd9dccca1ce65608b8
Evgeny Stepanov <evgenos@list.ru>
2020-10-09 20:28:22 +0300

    added the FeaturedRooms component to the Home page

--- src/client/components/pages/Home.js
+++ src/client/components/pages/Home.js
@@ -3,18 +3,20 @@ import { Link } from 'react-router-dom';
 import Hero from '../Hero';
 import Banner from '../Banner';
 import Services from '../Services';
+import FeaturedRooms from '../FeaturedRooms';
 
 export default function Home() {
   return (
     <>
       <Hero>
-        <Banner title='Luxurios rooms' subtitle='Deluxe rooms start at $299'>
+        <Banner title='Luxuriuos rooms' subtitle='Deluxe rooms start at $299'>
           <Link className='btn-primary' to='/rooms'>
             Our rooms
           </Link>
         </Banner>
       </Hero>
       <Services />
+      <FeaturedRooms />
     </>
   );
 }
