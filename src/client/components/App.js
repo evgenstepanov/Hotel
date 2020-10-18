0ee3a005b8e400f88d8ec7b01c3a184c4fd434e4
Evgeny Stepanov <evgenos@list.ru>
2020-10-18 15:29:28 +0300

    replaced the StoreProvider

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,32 +1,37 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import StoreProvider from '../store';
 
 import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import NotFound from './pages/NotFound';
 import NavBar from './NavBar';
-import '../index.scss'
+import Footer from './Footer';
+import '../index.scss';
 
 function App() {
   return (
     <>
-      <NavBar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/rooms">
-          <Rooms />
-        </Route>
-        <Route path="/rooms/:type">
-          <SingleRoom />
-        </Route> 
-        <Route>
-          <NotFound />
-        </Route>
-      </Switch>
+      <StoreProvider>
+        <NavBar />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/rooms'>
+            <Rooms />
+          </Route>
+          <Route path='/rooms/:type'>
+            <SingleRoom />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+        <Footer />
+      </StoreProvider>
     </>
   );
 }
--- src/client/index.js
+++ src/client/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
-import StoreProvider from './store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <StoreProvider>
-      <Router>
-        <App />
-      </Router>
-    </StoreProvider>
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
