edaf0cf69851c5e1b74d335bdd0d399794f8077f
Evgeny Stepanov <evgenos@list.ru>
2020-10-20 20:33:09 +0300

    added classes for tages

--- src/client/components/Loading.js
+++ src/client/components/Loading.js
@@ -4,8 +4,8 @@ import gif from '../img/gif/loading-gif.webp';
 export default function Loading() {
   return (
     <div className='loading'>
-      <h4>rooms data is loading...</h4>
-      <img src={gif} alt='' />
+      <img className='loading__img' src={gif} alt='' />
+      <h4>rooms data is loading</h4>
     </div>
   );
 }
