3cfc800e511a6210bdaa9a1b8554ee82a4e08cae
Evgeny Stepanov <evgenos@list.ru>
2020-10-20 17:47:49 +0300

    SingleRoom component

--- src/client/components/pages/SingleRoom.js
+++ src/client/components/pages/SingleRoom.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import Hero from '../Hero';
+import Banner from '../Banner';
+import { StoreContext } from '../../store';
 
 export default function SingleRoom() {
+  const {
+    rooms: [rooms],
+    getRoom,
+  } = React.useContext(StoreContext);
+  let { slug } = useParams();
+  const [singleRoom, setSingleRoom] = useState();
+
+  useEffect(() => {
+    let room = getRoom(slug);
+    setSingleRoom(room);
+  }, [rooms]);
+
+  if (singleRoom) {
+    const {
+      name,
+      description,
+      capacity,
+      size,
+      price,
+      extras,
+      breakfast,
+      pets,
+      images,
+    } = singleRoom;
+  }
   return (
-    <div>
-      SINGLE ROOM PAGE
-    </div>
-  )
+    <>
+      <Hero hero='rooms-hero'>
+        <Banner title={!singleRoom ? 'Error' : singleRoom.name}>
+          <Link className='btn-primary' to='/rooms'>
+            back to rooms
+          </Link>
+        </Banner>
+      </Hero>
+      {!singleRoom ? (
+        <div className='error-info'>
+          <h1 className='error-infp__title'>Sorry, there is not such room</h1>
+          <p>please, try another request</p>
+        </div>
+      ) : (
+        <div className='room-info'>ROOM_INFO</div>
+      )}
+    </>
+  );
 }
