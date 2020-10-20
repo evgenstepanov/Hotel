18948b08407df3852e28891c251a7f4f154e6d4c
Evgeny Stepanov <evgenos@list.ru>
2020-10-20 20:31:37 +0300

    created the RoomInfo component

---
+++ src/client/components/RoomInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function RoomInfo({ singleRoom }) {
+  const {
+    name,
+    description,
+    capacity,
+    size,
+    price,
+    extras,
+    breakfast,
+    pets,
+    images,
+  } = singleRoom;
+
+  return <div>ROOM_INFO</div>;
+}
