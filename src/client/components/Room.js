59ac83425e3b5c1d7c682e57332f434beeacc01a
Evgeny Stepanov <evgenos@list.ru>
2020-10-20 17:38:44 +0300

    added the button

--- src/client/components/Room.js
+++ src/client/components/Room.js
@@ -3,16 +3,20 @@ import { Link } from 'react-router-dom';
 import defaultImg from '../img/room-1.jpeg';
 
 export default function Room({ data }) {
-  const { name, type, images, price } = data;
+  const { name, slug, images, price } = data;
+
+  
   return (
     <article className='room'>
       <div className='image-container'>
         <img src={images[0] || defaultImg} alt={name} className='image-room' />
-        <div className='price image-container__price '>
+        <div className='price'>
           <h6 className='price__title'>${price}</h6>
           <p className='price__text'>per night</p>
         </div>
-        <Link to={`/rooms/${type}`}>LINK</Link>
+        <Link className='btn-primary room__link' to={`/rooms/${slug}`}>
+          {name}
+        </Link>
       </div>
     </article>
   );
