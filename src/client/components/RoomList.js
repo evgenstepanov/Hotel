ce4ccfd1caf238f9db77b3a976169cd33adce14a
Evgeny Stepanov <evgenos@list.ru>
2020-10-26 20:21:21 +0300

    RoomList component

---
+++ src/client/components/RoomList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StoreContext } from '../store';
+import Loading from './Loading';
+import Room from './Room';
+
+export default function RoomList({ type }) {
+  const {
+    loading: [loading],
+    featuredRooms: [featuredRooms],
+    sortedRooms: [sortedRooms],
+  } = React.useContext(StoreContext);
+
+  const rooms = type === 'sorted' ? sortedRooms : featuredRooms;
+
+  return (
+    <div className='center-container'>
+      <div className='rooms-container'>
+        {loading ? (
+          <Loading />
+        ) : (
+          rooms.map((room) => <Room key={room.id} data={room} />)
+        )}
+      </div>
+    </div>
+  );
+}
