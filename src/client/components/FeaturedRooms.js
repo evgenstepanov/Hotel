ac184bf780f45340ef7966aaf8d12701aa8eebe6
Evgeny Stepanov <evgenos@list.ru>
2020-10-26 20:23:36 +0300

    implemented the RoomList component

--- src/client/components/FeaturedRooms.js
+++ src/client/components/FeaturedRooms.js
@@ -1,27 +1,12 @@
 import React from 'react';
 import Title from './Title';
-import { StoreContext } from '../store';
-import Loading from './Loading';
-import Room from './Room';
+import RoomList from './RoomList';
 
-export default function FeaturedRooms() {
-  const {
-    featuredRooms: [featuredRooms],
-    loading: [loading],
-  } = React.useContext(StoreContext);
-
+export default function FeaturedRooms({ type }) {
   return (
     <section className='section'>
       <Title title='featured rooms' />
-      <div className='center-container'>
-        <div className='featured-rooms'>
-          {loading ? (
-            <Loading />
-          ) : (
-            featuredRooms.map((room) => <Room key={room.id} data={room} />)
-          )}
-        </div>
-      </div>
+      <RoomList type='featured' />
     </section>
   );
 }
