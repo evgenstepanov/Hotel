97af8e99a10c2a59d040368bd4a83de01e11201a
Evgeny Stepanov <evgenos@list.ru>
2020-10-28 19:11:40 +0300

    added select by type and by capacity

--- src/client/components/RoomFilter.js
+++ src/client/components/RoomFilter.js
@@ -3,7 +3,56 @@ import { StoreContext } from '../store';
 
 export default function RoomFilter() {
   const {
-    rooms: [rooms, setRooms],
+    rooms: [rooms],
+    filter: [filter],
+    handleChange,
+    getUnqueValue,
   } = React.useContext(StoreContext);
-  return <div>ROOMM FILTER</div>;
+
+  const {
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets,
+  } = filter;
+
+  const optionsType = ['all', ...getUnqueValue(rooms, 'type')];
+  const optionsCapacity = getUnqueValue(rooms, 'capacity');
+
+  return (
+    <form className='filter'>
+      <div className='filter-container'>
+        <label htmlFor='type'>Type of room</label>
+        <select name='type' id='type' value={type} onChange={handleChange}>
+          {optionsType.map(value => {
+            return (
+              <option key={value} value={value}>
+                {value}
+              </option>
+            );
+          })}
+        </select>
+        <label htmlFor='capacity'>Guests</label>
+        <select
+          name='capacity'
+          id='capacity'
+          value={capacity}
+          onChange={handleChange}
+        >
+          {optionsCapacity.map(value => {
+            return (
+              <option key={value} value={value}>
+                {value}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    </form>
+  );
 }
