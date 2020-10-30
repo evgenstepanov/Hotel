cd21d690bfbe58b8a02a0f059c032c9d4aebfadf
Evgeny Stepanov <evgenos@list.ru>
2020-10-30 14:29:16 +0300

    added filter inputs (price, size, pets? breakfast)

--- src/client/components/RoomFilter.js
+++ src/client/components/RoomFilter.js
@@ -27,6 +27,7 @@ export default function RoomFilter() {
   return (
     <form className='filter'>
       <div className='filter-container'>
+        {/* type of room */}
         <label htmlFor='type'>Type of room</label>
         <select name='type' id='type' value={type} onChange={handleChange}>
           {optionsType.map(value => {
@@ -37,6 +38,7 @@ export default function RoomFilter() {
             );
           })}
         </select>
+        {/* guests */}
         <label htmlFor='capacity'>Guests</label>
         <select
           name='capacity'
@@ -52,6 +54,47 @@ export default function RoomFilter() {
             );
           })}
         </select>
+        {/* price */}
+        <label htmlFor='price'>Price up to {price}</label>
+        <input
+          id='price'
+          name='price'
+          type='range'
+          min={minPrice}
+          max={maxPrice}
+          value={price}
+          onChange={handleChange}
+        />
+        {/* size */}
+        <label htmlFor='size'>room size </label>
+        <input
+          type='number'
+          name='minSize'
+          value={minSize}
+          onChange={handleChange}
+        />
+        <input
+          type='number'
+          name='maxSize'
+          value={maxSize}
+          onChange={handleChange}
+        />
+        {/* extras */}
+        <input
+          type='checkbox'
+          name='breakfast'
+          id='breakfast'
+          checked={breakfast}
+          onChange={handleChange}
+        />
+        <label htmlFor='breakfast'>breakfast</label>
+        <input
+          type='checkbox'
+          name='pets'
+          checked={pets}
+          onChange={handleChange}
+        />
+        <label htmlFor='breakfast'>pets</label>
       </div>
     </form>
   );
